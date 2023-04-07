3a857e395f6ded8b31092b7016c8e74879a30aa0
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:42:11 -0500

    Created CameraModels.cs in models folder.

---
+++ SecuritySystem/Models/CameraModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+    public class CameraSystem
+    {
+        public string CameraState { get; set; }
+
+        public void DetectTresspassersDuringOffHours() { }
+        public void DetermineIfPeopleArePresent() { }
+        public void DetectFireViaCamera() { }
+        public void NotifyControlCenterOnStatusChange() { }
+        public void RequestCameraFeed() { }
+    }
+
+    public class Camera
+    {
+        public void SendCameraFeedToCameraSystem() { }
+    }
+
+}
+
