9839bba479addc4357ec5642bfae8615a72f8410
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:34:06 -0500

    Created PhoneSystemModels.cs in models folder.

---
+++ SecuritySystem/Models/PhoneSystemModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+    public class PhoneSystem
+    {
+        public string CallingFromPhoneNumber { get; set; }
+        public List<string> ListOfNumbers { get; set; }
+
+        public void CallPolice() { }
+        public void CallOnCallEmployee() { }
+        public void CallFireDepartment() { }
+        public void CallChemicalDepartment() { }
+    }
+}
+
