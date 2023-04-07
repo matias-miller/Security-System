173a78893ee8ee3179c321546d3658d20cb478d9
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:36:39 -0500

    Created DoorModels.cs in models folder.

---
+++ SecuritySystem/Models/DoorModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+    public class Door
+    {
+        public bool IsLocked { get; set; }
+
+        public void LockDoor() { }
+        public void UnlockDoor() { }
+        public void SendStateOnChange() { }
+    }
+
+}
+
