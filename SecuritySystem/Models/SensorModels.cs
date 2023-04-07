7e5930e99de8fdf29768b7062e1ffb44be6393a3
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:39:16 -0500

    Created SensorModels.cs in models folder.

---
+++ SecuritySystem/Models/SensorModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+    public class Sensor
+    {
+        public bool IsActive { get; set; }
+        public bool IsGasSensor { get; set; }
+        public bool IsSmokeSensor { get; set; }
+
+        public void ActivateSensor() { }
+        public void DeactivateSensor() { }
+        public void SendStateOnChange() { }
+    }
+
+    public class GasSensor : Sensor
+    {
+        public void DetectGas() { }
+    }
+
+    public class SmokeSensor : Sensor
+    {
+        public void DetectSmoke() { }
+    }
+
+}
+
