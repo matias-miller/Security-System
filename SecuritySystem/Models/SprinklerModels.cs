7e1ed29eea590ab8010574d3407715ea8726f997
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:37:59 -0500

    Created SprinklerModels.cs in models folder.

---
+++ SecuritySystem/Models/SprinklerModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+    public class Sprinklers
+    {
+        public bool IsActive { get; set; }
+
+        public void ActivateSprinkler() { }
+        public void TurnOffSprinkler() { }
+        public void SendStateOnChange() { }
+    }
+
+}
+
