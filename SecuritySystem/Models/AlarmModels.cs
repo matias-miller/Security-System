3d946a2ee1ec37331d532fbbdee7c2888ebfce95
Matias Miller <matiasmiller@gmail.com>
2023-04-06 22:32:07 -0500

    Created AlarmModels.cs in models folder.

---
+++ SecuritySystem/Models/AlarmModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SecuritySystem.Models
+{
+
+    public class Alarm
+    {
+        public bool IsActive { get; set; }
+        public bool IsSmokeAlarm { get; set; }
+        public bool IsGasAlarm { get; set; }
+
+        public void ActivateAlarm() { }
+        public void DeactivateAlarm() { }
+        public void SendStateOnChange() { }
+    }
+
+}
