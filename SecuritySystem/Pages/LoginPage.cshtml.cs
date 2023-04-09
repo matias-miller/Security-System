dee6cde037827cdc3c56d375960ea06ce6ca5f05
Matias Miller <matiasmiller@gmail.com>
2023-04-08 19:21:01 -0500

    Added login page to Pages and navbar.

---
+++ SecuritySystem/Pages/LoginPage.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecuritySystem.Pages;
+
+public class LoginPageModel : PageModel
+{
+    private readonly ILogger<LoginPageModel> _logger;
+
+    public LoginPageModel(ILogger<LoginPageModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
